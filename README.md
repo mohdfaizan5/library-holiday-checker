140425295cfabadfec8b99a1833630b09a65375f
Mohammed Faizan <mohdfaizan.zprb2@gmail.com>
2024-04-05 07:47:40 +0530

    Government holidays working fine and full functional

--- README.md
+++ README.md
@@ -1,8 +1,39 @@
-# React + Vite
+"Republic Day" ,26-01-2024
+"Good Friday" ,29/03/2024
+"Chaitra Sukladi /Gudi Padava / Ugadi Cheti Chand" ,09-04-2024
+"Id-ul-Fitr" ,11/04/2024
+"Mahavir Jayanthi" ,21/04/2024
+"Buddha Purnima" ,23/05/2024
+"Id-ul-Zuha (Bakrid)" ,17/06/2024
+"Muharram" ,17/07/2024
+"Independence Day" ,15/08/2024
+"Ganesh Chaturthi" ,07/09/2024
+"Milad Un Nabi or ID-E-Milad
+(Birthday of Prophet Mohammad)" ,16/09/2024
+"Mahatma Gandhi's Birthday" ,02/10/2024
+"Dussehra [Mahanavarni]" ,11/10/2024
+"Dussehra (Vijay Dashmi)" ,12/10/2024
+"Diwali (Deepavali)" ,31/10/2024
+"Guru Nanak's Birthday" ,15/11/2024
+"Christmas Day" ,25/12/2024
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+[
+  { name: "Republic Day", date: "2024-01-26" },
+  { name: "Good Friday", date: "2024-03-29" },
+  { name: "Chaitra Sukladi /Gudi Padava / Ugadi Cheti Chand", date: "2024-04-09" },
+  { name: "Id-ul-Fitr", date: "2024-04-11" },
+  { name: "Mahavir Jayanthi", date: "2024-04-21" },
+  { name: "Buddha Purnima", date: "2024-05-23" },
+  { name: "Id-ul-Zuha (Bakrid)", date: "2024-06-17" },
+  { name: "Muharram", date: "2024-07-17" },
+  { name: "Independence Day", date: "2024-08-15" },
+  { name: "Ganesh Chaturthi", date: "2024-09-07" },
+  { name: "Milad Un Nabi or ID-E-Milad (Birthday of Prophet Mohammad)", date: "2024-09-16" },
+  { name: "Mahatma Gandhi's Birthday", date: "2024-10-02" },
+  { name: "Dussehra [Mahanavarni]", date: "2024-10-11" },
+  { name: "Dussehra (Vijay Dashmi)", date: "2024-10-12" },
+  { name: "Diwali (Deepavali)", date: "2024-10-31" },
+  { name: "Guru Nanak's Birthday", date: "2024-11-15" },
+  { name: "Christmas Day", date: "2024-12-25" }
+]
--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,35 @@ import { Helmet } from "react-helmet";
 // import { Schema } from "react-schema.org"; // Schema.org library
 import { Analytics } from "@vercel/analytics/react";
 
+const gov_holidays = [
+  { name: "Republic Day", date: "2024-01-26" },
+  { name: "Good Friday", date: "2024-03-29" },
+  {
+    name: "Chaitra Sukladi /Gudi Padava / Ugadi Cheti Chand",
+    date: "2024-04-09",
+  },
+  { name: "Id-ul-Fitr", date: "2024-04-11" },
+  { name: "Mahavir Jayanthi", date: "2024-04-21" },
+  { name: "Buddha Purnima", date: "2024-05-23" },
+  { name: "Id-ul-Zuha (Bakrid)", date: "2024-06-17" },
+  { name: "Muharram", date: "2024-07-17" },
+  { name: "Independence Day", date: "2024-08-15" },
+  { name: "Ganesh Chaturthi", date: "2024-09-07" },
+  {
+    name: "Milad Un Nabi or ID-E-Milad (Birthday of Prophet Mohammad)",
+    date: "2024-09-16",
+  },
+  { name: "Mahatma Gandhi's Birthday", date: "2024-10-02" },
+  { name: "Dussehra [Mahanavarni]", date: "2024-10-11" },
+  { name: "Dussehra (Vijay Dashmi)", date: "2024-10-12" },
+  { name: "Diwali (Deepavali)", date: "2024-10-31" },
+  { name: "Guru Nanak's Birthday", date: "2024-11-15" },
+  { name: "Christmas Day", date: "2024-12-25" },
+];
+
 const App = () => {
   return (
-    <div className="flex flex-col items-center h-[100vh] justify-center">
+    <div className="flex flex-col items-center min-h-[100vh] justify-center">
       <Analytics />
 
       <Helmet>
@@ -44,7 +70,7 @@ const App = () => {
       {/* <h1 className="[&::selection]:text-base-content relative col-start-1 row-start-1 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text [-webkit-text-fill-color:transparent] [&::selection]:bg-blue-700/20 [@supports(color:oklch(0%_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)] text-3xl font-bold" >Faizan</h1> */}
       <Calendar2 />
 
-      <div className="flex items-center gap-3 mt-10 mb-3">
+      <div className="flex items-center gap-3 mt-10 mb-10">
         <div className="flex items-center gap-2">
           Holiday{" "}
           <div className="p-1 border-4 border-red-700 rounded-full size-1"></div>
@@ -53,10 +79,14 @@ const App = () => {
           Today{" "}
           <div className="p-1 border-4 border-blue-700 rounded-full size-1"></div>
         </div>
+        <div className="flex items-center gap-2">
+          Gov Holiday{" "}
+          <div className="p-1 border-4 border-indigo-800 rounded-full size-1"></div>
+        </div>
       </div>
 
       {/* <div className="flex items-center gap-3 mt-10"></div> */}
-      <div>
+      <div className="pb-10">
         <h5>
           - Every <b>Monday</b>
         </h5>
@@ -67,11 +97,21 @@ const App = () => {
           - Fourth <b>Saturday</b>
         </h5>
         <h5>
-          - All <b>Government Holidays</b>
+          - All <b>Government Holidays list</b>
+          <div className="ml-10 text-sm font-light">
+            {gov_holidays.map(({ date, name }) => {
+              return (
+                <div key={date} className="max-w-80">
+                  - <span className="mr-2 font-bold">{date}</span> {name}
+                </div>
+              );
+            })}
+          </div>
         </h5>
+        <div className="px-3 py-2 mt-5 text-center text-black bg-red-200 rounded-full">Made with 💖<a className="underline " href="https://twitter.com/mohdfaizan_5/" target="_blank">mohdfaizan_5</a></div>
       </div>
       {/* <ChatBubble/> */}
-      <div
+      {/* <div
         role="alert"
         className="alert alert-warning bg-red-400 max-w-[55vw] flex max-h-12 mt-2 text-sm p-3 rounded-md"
       >
@@ -91,7 +131,7 @@ const App = () => {
         <span>
           <b>Note:</b> Government holidays not included
         </span>
-      </div>
+      </div> */}
     </div>
   );
 };
